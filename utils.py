431b1858073571e7ad3a5dd830af77c262429549
Pau Carrillo-Barbera <pau.carrillob@gmail.com>
2023-05-11 19:49:05 +0100

    erode labels using region props

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from skimage.measure import regionprops
+import skimage.morphology
 from scipy import ndimage
 from scipy.ndimage.morphology import distance_transform_edt
 from matplotlib.colors import LinearSegmentedColormap
@@ -88,6 +89,37 @@ def erode_labels2(image):
     image_eroded = np.array(image_eroded)
   
     return np.reshape(image_eroded, (image.shape[0]-2,image.shape[1]-2), order='F')
+
+# with regionprops
+# Converts a single label into a binary mask and erodes it
+def erode_mask(image, label_id, distance):   
+    binary_label_id = np.where(image == label_id, 1, 0) # crates binary image containing only the specified label
+    eroded = ndimage.binary_erosion(binary_label_id, iterations = distance) # erodes the binary mask
+    eroded_label_id = np.where(eroded == 1, label_id, 0) # creates label image containing only the specified label after erosion
+
+    return eroded_label_id
+
+# Erodes all the masks contained in a label image
+def erode_labels3(image, distance):
+    eroded_labels_list = [] # creates empty list to save images of the eroded labels
+    regions = regionprops(image)
+
+    # erodes every label and stores them as individual images
+    for i in range(len(regions)):
+        label_id = regions[i].label # gets the label id of the current region
+        eroded_label = erode_mask(image, label_id, distance) # creates label image containing only the specified label after erosion
+        eroded_labels_list.append(eroded_label) # stores the image on the list
+
+    # reconstructs the label image, now containing the eroded labels
+    eroded_labels_stack = np.stack(eroded_labels_list) # creates stack from list of images (numpy arrays)
+    image_eroded = np.max(eroded_labels_stack, axis = 0) # calculates the maximum projection to get back a 2D, labelled image
+
+    # reassigns labels if any mask is missing after erosion and rises a flag
+    if(len(regionprops(image_eroded)) < len(regions)):
+        image_eroded = skimage.morphology.label(image_eroded)
+        print('\033[93m[WARNING]: Objects missing after erosion. Consider reducing iterations (distance)')
+
+    return image_eroded
 
 # Get glasbey cmap
 def get_glasbey_cmap():
